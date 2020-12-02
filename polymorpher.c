a7c19169ab01a2981e69aab91d29fe6023aac0cf
Cedric Schwyter <cedricschwyter@bluewin.ch>
2020-12-02 21:20:33 +0100

    Add source structure

---
+++ polymorpher.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * The main entry point for the polymorph code.
+ * The idea is to modify the executable at runtime
+ * and shuffle bare assembly blocks around.
+ */
+int main() {
+
+    return 0;
+}
